33cbedec759594c56e6d876fb1f06a6dfe91ad7d
Raul Barros <raulhbarros@outlook.com>
2024-02-13 18:34:08 +0000

    feat:notes modals created

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,24 @@
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
+import { SearchNotes } from './components/search-notes'
+
+const note = {
+  date: new Date(),
+  content: "hsuahsuahsuah"
+}
+
 export function App() {
   return (
-<h1>Hello world</h1>
+    <div className='mx-auto max-w-6xl my-12 space-y-6'>
+      <SearchNotes/>
+
+      <div className='grid grid-cols-3 auto-rows-[250px] gap-6'>
+        <NewNoteCard />
+        <NoteCard note={note}/>
+               
+      </div>
+
+    </div>
   )
 }
 
